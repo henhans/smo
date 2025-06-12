fb88a678bb0efc81f7068581e3218baf887cb379
Tsung-Han Lee <henhans74716@gmail.com>
2025-06-12 09:33:48 +0800

    change folder name

--- pm3m_to_imma/0_60/run.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/bash
-
-nohup mpirun --bind-to numa -np 1 python3 -u main.py grisb_config.toml &
--- pm3m_to_imma/0_65/autorm.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/bash
-
-rm -r out CRASH *.out *.wout *.err *.dat* *.h5 *.amn *.chk *.eig *.mmn *.nnkp *.mat *.kpt *.gnu
