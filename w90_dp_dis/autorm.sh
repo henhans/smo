432135173f70e94a79174c825e3873af7dfe4155
Tsung-Han Lee <henhans74716@gmail.com>
2025-12-20 11:50:42 +0800

    remove

--- w90_dp_dis/autorm.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/bash
-rm -r out
-rm *.amn *.eig *.nnkp *_hr.dat *.mat *.chk *.mmn *.wout *.dat *.out *.err *.gnu *.kpt
--- w90_dp_dis/convert_wannier.py
+++
@@ -1,5 +0,0 @@
-#%%
-from triqs_ghostGA.wannier90 import Wannier90Converter
-Converter = Wannier90Converter(seedname='smo', rot_mat_type='none')
-Converter.convert_dft_input()
-# %%
